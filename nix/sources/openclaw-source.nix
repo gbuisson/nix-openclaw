0cead6a5d8b08dc86d43f624927d6b02e80b748a
Guillaume Buisson <guillaume@gbuisson.com>
2026-04-01 18:18:24 +0200

    feat: pin OpenClaw source to v2026.3.31 (213a704)

--- nix/sources/openclaw-source.nix
+++ nix/sources/openclaw-source.nix
@@ -2,7 +2,7 @@
 {
   owner = "openclaw";
   repo = "openclaw";
-  rev = "823a09acbefc893f4ca143d90898b41a482c7a36";
-  hash = "sha256-GMpKjPiwgM4bUC1a0a8xV8sujO/6AsgLdXMKdvqrlps=";
-  pnpmDepsHash = "sha256-NcQZmaOny8eBB/z3FheDjYm3++lee5CujCGByLG/aHU=";
+  rev = "213a704b71f4996dc82a583288ee53785215f627";
+  hash = "sha256-pUABN6qjzNZ/0MgA/+pWjT9efhcm/5zicwQ/BRbXVDo=";
+  pnpmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
 }
